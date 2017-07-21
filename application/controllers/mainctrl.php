88e424672ec91cc1f227eda84a7f3c70bd873b10
Akkshay Lawrence <cruzer6ryc4@gmail.com>
2017-07-21 09:47:21 +0530

    php beautification.

--- application/controllers/mainctrl.php
+++ application/controllers/mainctrl.php
@@ -1,48 +1,53 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-class Mainctrl extends CI_Controller {
-	public function index(){
-		$data = $this->init->initPath('/mainctrl');
-		$data += $this->session->userdata();
-		if($this->session->userdata('loggedIn')){
-			$this->load->view('pages/main',$data);
-		}else{
-			$this->load->view('pages/home',$data);
-		}
-	}
-	public function userLoginProcess() {
-		$this->load->model('User');
-		$this->load->model('Init');
-		$data = $this->init->initPath('/mainctrl');
-		$username = $this->input->post('username');
-		$password = $this->input->post('password');
-		$userData = $this->User->checkUser($username, $password);
-		log_message('info',print_r($userData,TRUE));
-		if ($userData['authStatus'] == FALSE) {
-			echo $userData['authStatus'].","."Incorrect username or password";
-		}elseif ($userData['authStatus'] == TRUE) {
-			$newdata = array(
-				'uid'  => $userData['uid'],
-				'name'  => $userData['name'],
-				'loggedIn' => TRUE
-			);
-			$this->session->set_userdata($newdata);
-			log_message('info',print_r($_SESSION,TRUE));
-			exit;
-		}
-	}
-	public function main(){
-		$data = $this->init->initPath('/mainctrl');
-		$data += $this->session->userdata();
-		if($this->session->userdata('loggedIn')){
-			$this->load->view('pages/main',$data);
-		}else{
-			$this->load->view('pages/home',$data);
-		}
-	}
-	public function logout() {
-	$data = $this->init->initPath ('/mainctrl');
-	$this->session->sess_destroy();
-	redirect(base_url(),'refresh');
-	}
+defined('BASEPATH') or exit('No direct script access allowed');
+class Mainctrl extends CI_Controller
+{
+    public function index()
+    {
+        $data = $this->init->initPath('/mainctrl');
+        $data += $this->session->userdata();
+        if ($this->session->userdata('loggedIn')) {
+            $this->load->view('pages/main', $data);
+        } else {
+            $this->load->view('pages/home', $data);
+        }
+    }
+    public function userLoginProcess()
+    {
+        $this->load->model('User');
+        $this->load->model('Init');
+        $data = $this->init->initPath('/mainctrl');
+        $username = $this->input->post('username');
+        $password = $this->input->post('password');
+        $userData = $this->User->checkUser($username, $password);
+        log_message('info', print_r($userData, true));
+        if ($userData['authStatus'] == false) {
+            echo $userData['authStatus'].","."Incorrect username or password";
+        } elseif ($userData['authStatus'] == true) {
+            $newdata = array(
+                'uid'  => $userData['uid'],
+                'name'  => $userData['name'],
+                'loggedIn' => true
+            );
+            $this->session->set_userdata($newdata);
+            log_message('info', print_r($_SESSION, true));
+            exit;
+        }
+    }
+    public function main()
+    {
+        $data = $this->init->initPath('/mainctrl');
+        $data += $this->session->userdata();
+        if ($this->session->userdata('loggedIn')) {
+            $this->load->view('pages/main', $data);
+        } else {
+            $this->load->view('pages/home', $data);
+        }
+    }
+    public function logout()
+    {
+        $data = $this->init->initPath ('/mainctrl');
+        $this->session->sess_destroy();
+        redirect(base_url(), 'refresh');
+    }
 }
