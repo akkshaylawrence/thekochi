1b00650a212a6ff502d45df05b7463f72d8834d8
Akkshay Lawrence <cruzer6ryc4@gmail.com>
2017-07-22 12:27:53 +0530

    Started Work on Dashboard.

--- application/views/pages/main.php
+++ application/views/pages/main.php
@@ -18,40 +18,21 @@
 			<img class="brand-logo" src="<?= $images; ?>/thekochi.png" alt="theKochi">
 		</a>
 		<ul id="nav-mobile" class="right hide-on-med-and-down">
-			<li><a href="<?=site_url("mainctrl/logout")?>">LOGOUT</a></li>
+			<li><a href="<?=site_url(" mainctrl/logout ")?>">LOGOUT</a></li>
 		</ul>
 	</div>
 </nav>
 
-<body>
+<body class="cyan">
 	<div class="container">
-		<div class="card">
-			<div class="card-content">
-				<h2>Please fill the form.</h2>
-			</div>
-			<div id="inform" class="card-content grey lighten-4">
-				<div id="info">
-					<form action="">
-						<div id="formrow" class="row">
-							<div class="input-field col s6">
-								<input id="fname" type="text" placeholder="Enter your First Name">
-								<label for="fname">First Name</label>
-							</div>
-							<div class="input-field col s6">
-								<input id="lname" type="text" placeholder="Enter your Last Name">
-								<label for="lname">Last Name</label>
-							</div>
-						</div>
-						<div id="formrow" class="row">
-							<div class="input-field col s6">
-								<input id="phone" type="text" class="validate" placeholder="Enter your Mobile Number">
-								<label data-error="Invalid Mobile Number" for="phone">Mobile Number</label>
-							</div>
-						</div>
-						<div id="formrow" class="row">
-							<a id="loginbtn" class="right waves-effect waves-green btn-flat">Submit</a>
-						</div>
-					</form>
+		<div class="row">
+			<div class="col s12 m6 l6">
+				<div class="card z-depth-2">
+					<div class="card-content center">
+						<h3>Name</h3>
+					</div>
+					<div id="inform" class="card-content grey lighten-4">
+					</div>
 				</div>
 			</div>
 		</div>
@@ -61,5 +42,6 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.99.0/js/materialize.min.js"></script>
 <script src="<?= $js ?>/jquery-ui.min.js"></script>
 <script src="<?= $js ?>/register.js"></script>
+<script src="<?= $js ?>/data.js"></script>
 
 </html>
